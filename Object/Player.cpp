d17cab4617593684bf6258ff3c96a882f28e397c
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2022-11-29 08:29:00 +0000

    test changes

--- Object/Player.cpp
+++ Object/Player.cpp
@@ -9,3 +9,17 @@ float Player::GetRotationSpeed()
 {
 	return rotationSpeed;
 }
+
+glm::vec3 Player::GetVelocity()
+{
+	return currentVelocity;
+}
+
+void Player::AddForce(glm::vec3 force)
+{
+	this->currentVelocity += force;
+}
+void Player::UpdatePosition(float deltaTime)
+{
+	objectPosition += currentVelocity;
+}
--- Object/Player.h
+++ Object/Player.h
@@ -6,6 +6,7 @@ class Player : public Object
 private:
 	const float rotationSpeed = 0.002f;
 	const float speed = 0.005;
+	glm::vec3 currentVelocity;
 	//public members
 public:
 
@@ -16,5 +17,8 @@ private:
 public:
 	float GetSpeed();
 	float GetRotationSpeed();
+	glm::vec3 GetVelocity();
+	void AddForce(glm::vec3 force);
+	void UpdatePosition(float deltaTime);
 };
 
